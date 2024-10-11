c48552966590d4de0074a7fbc23c138d900d0dac
Fenneko0 <vlad.tk.dev@gmail.com>
2024-10-11 16:04:44 +0300

    Updated chromedriver

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,46 +1,41 @@
 exports.config = {
-    runner: 'local',
-    specs: [
-        './test/specs/**/*.js'
-    ],
-    exclude: [
-        // 'path/to/excluded/files'
-    ],
-    maxInstances: 10,
-    headless: true,
-    capabilities: [
-        {
-        maxInstances: 5,
-        browserName: 'chrome',
-        acceptInsecureCerts: true,
-        'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu']
-        }
+  runner: "local",
+  specs: ["./test/specs/**/*.js"],
+  exclude: [
+    // 'path/to/excluded/files'
+  ],
+  maxInstances: 10,
+  headless: true,
+  capabilities: [
+    {
+      maxInstances: 5,
+      browserName: "chrome",
+      acceptInsecureCerts: true,
+      "goog:chromeOptions": {
+        args: ["headless", "disable-gpu"],
+      },
     },
     {
-        maxInstances: 5,
-        browserName: 'firefox',
-        acceptInsecureCerts: true,
-        'moz:firefoxOptions': {
-            args: ['-headless']
-        }
-    }
-],
-    logLevel: 'error',
-    bail: 0,
-    baseUrl: 'https://cnt-bb591083-c9d5-4bfe-9d40-34232dee1bea.containerhub.tripleten-services.com/',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    services: [
-        'chromedriver', 
-        'geckodriver', 
-        'intercept', 
-    ],
-    framework: 'mocha',
-    reporters: ['spec'],
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
+      maxInstances: 5,
+      browserName: "firefox",
+      acceptInsecureCerts: true,
+      "moz:firefoxOptions": {
+        args: ["-headless"],
+      },
     },
-}
+  ],
+  logLevel: "error",
+  bail: 0,
+  baseUrl:
+    "https://cnt-eb3204ef-2496-4566-ae55-945a2e625eab.containerhub.tripleten-services.com/",
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: ["chromedriver", "geckodriver", "intercept"],
+  framework: "mocha",
+  reporters: ["spec"],
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+};
