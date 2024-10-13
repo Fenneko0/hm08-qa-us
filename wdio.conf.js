7a473d6905d040ec64e6cd9196a73dd41c384f51
Fenneko0 <vlad.tk.dev@gmail.com>
2024-10-13 15:58:35 +0300

    Finial commit and push for submitting the project

--- wdio.conf.js
+++ wdio.conf.js
@@ -27,7 +27,7 @@ exports.config = {
   logLevel: "error",
   bail: 0,
   baseUrl:
-    "https://cnt-30dc7343-591f-42c0-bd5e-92e6fa70a1d2.containerhub.tripleten-services.com/",
+    "https://cnt-30fc8a1d-7166-4dd9-b308-a93ebcf3f27e.containerhub.tripleten-services.com/",
   waitforTimeout: 10000,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
